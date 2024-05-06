ca03413fe8bffa6a9787963c809608e4efa0d114
George Bobby <georgebobbyawesome@gmail.com>
2024-05-06 18:23:45 +0530

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,45 @@
+import { useState } from "react";
 import Message from "./Message";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
 function App() {
 	let cities = ["mumbai", "kolakta", "chennai", "bengaluru"];
+
 	const handleSelectItem = (item: string) => {
 		return console.log(item);
 	};
+
+	const [alertVisible, setAlertVisibility] = useState(false);
+
 	return (
 		<>
-			{/* <Message />
-			<ListGroup
+			{/* <Message /> */}
+			{/* <ListGroup
 				cities={cities}
 				heading="Cities List"
 				onSelectItem={handleSelectItem}
-			/>
-			<Alert>
+			/> */}
+			{/* <Alert>
 				Hello <span>World</span>
 			</Alert> */}
 
-			<Button
+			{/* <Button
 				onClick={() => console.log("Clicked")}
 				color="danger"
+				>
+				Button
+			</Button> */}
+			{alertVisible && (
+				<Alert onClose={() => setAlertVisibility(false)}>
+					Hello Alert
+				</Alert>
+			)}
+			<Button
+				onClick={() => setAlertVisibility(true)}
+				color="primary"
 			>
-				My Button
+				Button
 			</Button>
 		</>
 	);
